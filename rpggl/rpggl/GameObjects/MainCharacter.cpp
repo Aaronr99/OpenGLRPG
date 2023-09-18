7c2e6984aec1a8f9b99ff0f9040f894128ef2728
Aaronr99 <aaron.rod2399@gmail.com>
2023-09-18 14:43:32 -0300

    feat: Movement relative to camera angle

--- rpggl/rpggl/GameObjects/MainCharacter.cpp
+++ rpggl/rpggl/GameObjects/MainCharacter.cpp
@@ -9,30 +9,28 @@ void MainCharacter::Update() {
 	glm::vec3 front = NormalizeToOneComponent(camera->Front);
 	glm::vec3 right = NormalizeToOneComponent(camera->Right);
 
-
-
 	if (g_InputManager.GetKeyDown(GLFW_KEY_W))
 	{
 		transform.Move(previousPos + front);
-		transform.Rotate(glm::vec3(0.0f, 0.0f, 0.0f));
+		//transform.Rotate(glm::vec3(0.0f, 0.0f, 0.0f));
 		DebugPosition(transform.position);
 	}
 	if (g_InputManager.GetKeyDown(GLFW_KEY_S))
 	{
 		transform.Move(previousPos - front);
-		transform.Rotate(glm::vec3(0.0f, 180.0f, 0.0f));
+		//transform.Rotate(glm::vec3(0.0f, 180.0f, 0.0f));
 		DebugPosition(transform.position);
 	}
 	if (g_InputManager.GetKeyDown(GLFW_KEY_A))
 	{
-		transform.Move(previousPos + right);
-		transform.Rotate(glm::vec3(0.0f, 90.0f, 0.0f));
+		transform.Move(previousPos - right);
+		//transform.Rotate(glm::vec3(0.0f, 90.0f, 0.0f));
 		DebugPosition(transform.position);
 	}
 	if (g_InputManager.GetKeyDown(GLFW_KEY_D))
 	{
-		transform.Move(previousPos - right);
-		transform.Rotate(glm::vec3(0.0f, -90.0f, 0.0f));
+		transform.Move(previousPos + right);
+		//transform.Rotate(glm::vec3(0.0f, -90.0f, 0.0f));
 		DebugPosition(transform.position);
 	}
 }
@@ -46,24 +44,37 @@ glm::vec3 NormalizeToOneComponent(const glm::vec3& inputVec3)
 {
 	// Encuentra el índice de la componente más grande
 	int maxIndex = 0;
-	float maxValue = inputVec3.x;
 
-	if (inputVec3.y > maxValue)
+	float xValue = inputVec3.x;
+	if (xValue < 0) xValue *= -1;
+	float yValue = inputVec3.y;
+	if (yValue < 0) yValue *= -1;
+	float zValue = inputVec3.z;
+	if (zValue < 0) zValue *= -1;
+
+	float maxValue = xValue;
+
+	if (yValue > maxValue)
 	{
 		maxIndex = 1;
-		maxValue = inputVec3.y;
+		maxValue = yValue;
 	}
 
-	if (inputVec3.z > maxValue)
+	if (zValue > maxValue)
 	{
 		maxIndex = 2;
 	}
 
-	// Crea un nuevo glm::vec3 con todas las componentes en cero
 	glm::vec3 result(0.0f);
 
-	// Establece la componente más grande en uno
-	result[maxIndex] = 1.0f;
+	if (inputVec3[maxIndex] < 0)
+	{
+		result[maxIndex] = -1.0f;
+	}
+	else
+	{
+		result[maxIndex] = 1.0f;
+	}
 
 	return result;
 }
